73579a79125221a1744192366107a8d5d65b9c86
Jaskaran Chahal <Jschahal@uncg.edu>
2024-11-10 19:12:49 -0500

    reset

--- hw/hw6/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const jokeController = require('../controllers/jokeController');
-
-router.get('/categories', jokeController.getCategories);
-router.get('/joke/:category', jokeController.getJokesByCategory);
-router.post('/joke/new', jokeController.addJoke);
-
-module.exports = router;
